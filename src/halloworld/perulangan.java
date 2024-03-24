a634ccc7cc82d76c9515f7a295ad125ecfa2674e
EliteBook <EliteBook@holla>
2024-03-25 00:26:35 +0700

    PBO

---
+++ src/halloworld/perulangan.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package halloworld;
+
+/**
+ *
+ * @author EliteBook
+ */
+public class perulangan {
+    
+    public static void main(String[] args){
+        
+        int i,j,k;
+        
+        
+        for(i=0;i<=4;i++)
+        {
+            for(j=4;j>=i;j--)
+            {
+            System.out.print(" ");
+            }
+        
+        for(k=0;k<=i;k++){
+            
+            System.out.print(" *");
+        }System.out.println("");
+        }
+    }
+}
